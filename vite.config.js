d0bf2379724bc7b16bf29585ae8a18265be19a17
Ahmed Ben Makhlouf <101886914+Ahmedbm99@users.noreply.github.com>
2025-11-24 22:05:08 +0100

    deploy

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 // https://vite.dev/config/
 export default defineConfig({
+    base: "/Kortibelt/",
     resolve: {
     alias: {
     "@": path.resolve(__dirname, "src"),
