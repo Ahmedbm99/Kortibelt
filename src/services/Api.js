3d1ac11697c01a666d2d759a0b24c885c8d8123b
Ahmed Ben Makhlouf <101886914+Ahmedbm99@users.noreply.github.com>
2026-02-07 01:50:04 +0100

    ok

--- src/services/Api.js
+++ src/services/Api.js
@@ -6,7 +6,7 @@ const Api = () => {
   return axios.create({
    // baseURL: `http://localhost:8080/apiv1`,
    
- baseURL: `https://courroie-back-9v2h7iwx3-ahmedbm99s-projects.vercel.app/apiv1`,
+ baseURL: `https://courroie-back-paofos30o-ahmedbm99s-projects.vercel.app/apiv1`,
     headers: {
       "Content-Type": "application/json",
     },
